1b3336f5fc11b7cd06fb64d94fa31c922f64e4e0
Bethelhem Tarekegn <betitarekegn12@gmail.com>
2023-03-13 23:38:25 +0300

    route added

---
+++ routes/SongRoute.js
@@ -0,0 +1,16 @@
+// routes/songs.js
+
+const express = require('express');
+const router = express.Router();
+const songController = require('../controllers/SongController');
+
+router.get('/songs', songController.getAllSongs);
+router.post('/addsong', songController.createSong);
+router.get('/song/:id', songController.getSongById, (req, res) => {
+  res.json(res.song);
+});
+router.patch('/editsong/:id', songController.getSongById, songController.updateSong);
+router.delete('/deletesong/:id', songController.getSongById, songController.deleteSong);
+router.get('/status', songController.status);
+
+module.exports = router;
